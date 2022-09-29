7f69ab986d59df4cd64562e77c34e91aff71e8b1
Ayaskanta Pandia <ayaskanta.pandia@VEROLT.COM>
2022-09-29 11:26:35 +0530

    changes

--- 1.cpp
+++ 1.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 int main(){
     cout<<"Hello Nancy";
-<<<<<<< HEAD
-    cout<<"I have made changes into branch";
-=======
-    cout<<"I have made changes in demo1 main";
->>>>>>> 5dc3315a9e0d9579614d290f14cd77901c0ba7fc
     cout<<"git hub";
     cout<<"github2";
     return 0;
