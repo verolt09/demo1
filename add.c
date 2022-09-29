4c6ddd51987696103cb782f5993a865cb0a8de2e
Ayaskanta Pandia <ayaskanta.pandia@VEROLT.COM>
2022-09-29 11:36:39 +0530

    deleted 2 lines in branch

--- add.c
+++ add.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 int main() {
 
-    int number1, number2, sum, sub, mul;
+    int number1, number2, sum;
 
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
     sum = number1 + number2;
-    sub = number1 - number2;
-    mul = number1 * number2;
+   
 
     printf("%d + %d = %d", number1, number2, sum);
-    printf("%d + %d = %d", number1, number2, sub);
-    printf("%d + %d = %d", number1, number2, mul);
+ 
     return 0;
 }
